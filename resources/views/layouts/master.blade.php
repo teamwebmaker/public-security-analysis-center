fc7daaf4e5e1013b6b210b9dcbad8be8c82eb5f8
Akimishvili <liliana.aqimishvili@gmail.com>
2024-01-06 01:22:19 +0400

    project base structure

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+ @include('partials.head')
+</head>
+<body>
+<div class="wrapper">
+    @include('partials.header')
+    @yield('main')
+    @include('partials.footer')
+</div>
+
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+<script defer src="{{ asset('scripts/app.js') }}"></script>
+@yield('scripts')
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title',  'Home Page')
+@section('main')
+<main>
+    <h1>Home page</h1>
+</main>
+@endsection
+@section('scripts')
+    <script></script>
+@endsection
