2292351debd9d1115688d73623cdf9eb577262a3
Bekauridev <beqaurigiorgi31@gmail.com>
2025-11-11 20:03:41 +0400

    update dashboard  sidebar header and company leader dashboard

--- resources/views/management/company-leader/dashboard.blade.php
+++ resources/views/management/company-leader/dashboard.blade.php
@@ -23,12 +23,11 @@
 		<div class="my-3 shadow-sm rounded-3 overflow-hidden ">
 			<x-shared.table :items="$tasks" :headers="['#', 'სტატუსი', 'შემსრულებელი', 'ფილიალი', 'სერვისი', 'დოკუმენტი', 'სამუშაოს დაწყება', 'სამუშაოს დასრულება',]" :rows="$userTableRows" :sortableMap="['სამუშაოს დაწყება' => 'start_date', 'სამუშაოს დასრულება' => 'end_date',]" :tooltipColumns="['branch', 'service']" :actions="false" />
 		</div>
-	@else
-		<x-ui.empty-state-message :resourceName="null" :overlay="false" />
+
 	@endif
 
 	<!-- company & branches-->
-	<div class="accordion" id="companyAccordion">
+	<div class="accordion mt-3" id="companyAccordion">
 		@foreach ($userCompanies as $index => $company)
 			@php
 				$accordionId = 'company-' . $company->id;
